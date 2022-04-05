fd5f8cdd31c84638f097559776efa8c774e382f8
Roma Patel <roma.p@simformsolutions.com>
2022-04-05 13:39:30 +0530

    whatsappUI

---
+++ app/src/main/java/com/example/androidoverview/ChatFragmentActivity.kt
@@ -0,0 +1,29 @@
+package com.example.androidoverview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_chat_activity.whatsAppChatRecycler
+
+class ChatFragmentActivity : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chat_activity, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        whatsAppChatRecycler.layoutManager = LinearLayoutManager(context)
+        val adapter = ChatAdapterClass(chatDetails)
+        whatsAppChatRecycler.adapter = adapter
+
+    }
+
+}
--- app/src/main/java/com/example/androidoverview/MainActivity.kt
+++ app/src/main/java/com/example/androidoverview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.btnChapterA
+import kotlinx.android.synthetic.main.activity_main.btnChapterC
 import kotlinx.android.synthetic.main.activity_main.btnChapterB
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btnChapterA.setOnClickListener {
             val intent = Intent(this,RegistrationFormActivity::class.java)
+            startActivity(intent)
+        }
+        btnChapterC.setOnClickListener{
+            val intent = Intent(this, RecyclerAndListExamplesActivity::class.java)
             startActivity(intent)
         }
         btnChapterB.setOnClickListener{
