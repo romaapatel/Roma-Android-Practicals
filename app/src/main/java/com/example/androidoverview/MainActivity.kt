9d31405b17b53634f52bb427049962e403fc828b
Roma Patel <roma.p@simformsolutions.com>
2022-07-08 16:11:15 +0530

    App center

--- app/src/main/java/com/example/androidoverview/MainActivity.kt
+++ app/src/main/java/com/example/androidoverview/MainActivity.kt
@@ -5,16 +5,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidoverview.architecture.MVCArchitectureActivity
 import com.example.androidoverview.intentExample.IntentAndFragmentActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.btnArchitectureChapter
 import kotlinx.android.synthetic.main.activity_main.btnChapterA
 import kotlinx.android.synthetic.main.activity_main.btnChapterB
 import kotlinx.android.synthetic.main.activity_main.btnChapterD
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "a77f8a2f-5679-4294-9722-c43f00750157",
+            Analytics::class.java, Crashes::class.java
+        )
 
         btnChapterA.setOnClickListener {
             val intent = Intent(this@MainActivity, RegistrationFormActivity::class.java)
